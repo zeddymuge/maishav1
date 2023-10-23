a2000f4c1bdd71fac059c5467684276049d9de40
zeddy muge <cheroozeddy@gmail.com>
2023-10-23 13:57:32 +0300

    second

--- libs/web/components/contacts/src/lib/contact/contact.component.ts
+++ libs/web/components/contacts/src/lib/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ReactiveFormsModule , FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { ContactService } from '../contact.service';
